3b2bbb74dcd2834191939e0e22912882b3017b30
Szczyp <qb@szczyp.com>
2019-09-04 17:17:15 +0200

    Add Direnv support. Remove shell.nix.

--- default.nix
+++ default.nix
@@ -2,15 +2,22 @@
 
 let
   python = import ./requirements.nix { inherit pkgs; };
-in python.mkDerivation {
-  name = "ebook-1.0.3";
-  src = ./.;
-  buildInputs = [];
-  propagatedBuildInputs =
-    (builtins.attrValues python.packages)
-    ++ (with pkgs; [
-      pandoc
-      kindlegen
-      readability
-    ]);
-}
+
+  external-dependencies = (with pkgs; [
+    pandoc
+    kindlegen
+    readability
+  ]);
+
+  drv = python.mkDerivation {
+    name = "ebook-1.0.3";
+    src = ./.;
+    buildInputs = [];
+    propagatedBuildInputs = (builtins.attrValues python.packages) ++ external-dependencies;
+  };
+
+  dev-env = pkgs.mkShell {
+    buildInputs = [ python.interpreter ] ++ external-dependencies;
+  };
+in
+if pkgs.lib.inNixShell then dev-env else drv
