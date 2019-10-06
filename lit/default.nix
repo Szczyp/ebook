18b68e045516679a2d6b6a774e5b4db8a4784db2
Szczyp <qb@szczyp.com>
2019-10-06 21:27:57 +0200

    Add lit.

---
+++ lit/default.nix
@@ -0,0 +1,68 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  name = "lit";
+  version = "1.0.0";
+  nodejs = pkgs.nodejs-10_x;
+  nodeHeaders = pkgs.fetchurl {
+    url = "https://nodejs.org/download/release/v${nodejs.version}/node-v${nodejs.version}-headers.tar.gz";
+    sha256 = "0m2yp73rig154iazvs9d1zmi2k32lci5i76pz4idkxg0pzqx2089";
+  };
+  rdkafka = with pkgs; stdenv.mkDerivation rec {
+    pname = "rdkafka";
+    version = "1.1.0";
+
+    src = fetchFromGitHub {
+      owner = "edenhill";
+      repo = "librdkafka";
+      rev = "v${version}";
+      sha256 = "03h4yxnbnig17zapnnyvvnh1bsp0qalvlpb4fc3bpvs7yj4d8v25";
+    };
+
+    nativeBuildInputs = [ pkgconfig ];
+    buildInputs = [ zlib perl python openssl ];
+
+    NIX_CFLAGS_COMPILE = "-Wno-error=strict-overflow";
+
+    postPatch = ''
+      patchShebangs .
+    '';
+  };
+  yarn = pkgs.yarn.override { inherit nodejs; };
+  yarn2nix = pkgs.callPackage (pkgs.fetchFromGitHub {
+      owner  = "moretea";
+      repo   = "yarn2nix";
+      rev    = "7effadded30d611a460e212cb73614506ef61c52";
+      sha256 = "0r4ww3mjx3n7bn2v1iav33liyphyr2x005ak6644qs15bp4bn3cr";
+  }) { inherit pkgs nodejs yarn; };
+  yarn-install = pkgs.writeScriptBin "yarn-install" ''
+    #!/usr/bin/env bash
+    export LIBRDKAFKA=${rdkafka}
+    export npm_config_tarball=${nodeHeaders}
+    yarn install --check-files
+  '';
+  drv = yarn2nix.mkYarnPackage {
+    name = "${name}-${version}";
+    src = ./.;
+    packageJSON = ./package.json;
+    yarnLock = ./yarn.lock;
+    yarnNix = ./yarn.nix;
+    pkgConfig = {
+      node-rdkafka = {
+        buildInputs = with pkgs; [ python binutils gcc gnumake nodePackages.node-gyp ];
+        postInstall = ''
+          export LIBRDKAFKA=${rdkafka}
+          node-gyp rebuild --tarball=${nodeHeaders}
+        '';
+      };
+    };
+  };
+  shell = pkgs.mkShell {
+    buildInputs = with pkgs; [ python binutils gcc gnumake ] ++ [ nodejs yarn yarn2nix.yarn2nix yarn-install ];
+  };
+    image = pkgs.dockerTools.buildLayeredImage {
+      inherit name;
+      tag = "latest";
+      config.Cmd = [ "${drv}/bin/${name}"];
+    };
+in
+drv // { inherit shell image; }
---
+++ lit/index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const Kafka = require("node-rdkafka");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const Readability = require("readability");
+const process = require("process");
+
+const broker = process.env.KAFKA_BOOTSTRAP_SERVERS || "localhost:9092";
+
+const consumer = Kafka.KafkaConsumer.createReadStream({
+  "group.id": "lit",
+  "metadata.broker.list": broker
+}, {}, {
+  topics: [ "fetched_html" ]
+});
+
+const producer = Kafka.Producer.createWriteStream({
+  "metadata.broker.list": broker
+}, {}, {
+  topic: "articles"
+});
+
+consumer.on("data", function(message) {
+  const request = JSON.parse(message.value);
+  const doc = new JSDOM(request.html, {url: request.url});
+  const reader = new Readability(doc.window.document);
+  let article = reader.parse();
+
+  let resp = JSON.stringify({...article, ...request});
+  producer.write(Buffer.from(resp));
+});
