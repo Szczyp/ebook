7c76e7a3b0f1aa08629fa1786a901c98512f3c3f
Szczyp <qb@szczyp.com>
2019-10-07 01:57:01 +0200

    Hlint pubes

--- pubes/Main.hs
+++ pubes/Main.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE DeriveGeneric, DuplicateRecordFields, NamedFieldPuns,
-             OverloadedStrings, RecordWildCards, StrictData #-}
+{-# LANGUAGE DuplicateRecordFields, NamedFieldPuns, OverloadedStrings,
+             RecordWildCards, StrictData #-}
 
 module Main where
 
@@ -7,18 +7,20 @@ import Article
 import Epub
 
 import           Control.Error.Util
-import           Control.Monad          (void)
-import           Control.Monad.IO.Class (liftIO)
-import           Control.Monad.Logger   (runNoLoggingT, runStdoutLoggingT)
+import           Control.Monad              (void)
+import           Control.Monad.IO.Class     (liftIO)
+import           Control.Monad.Logger       (runNoLoggingT, runStdoutLoggingT)
+import           Control.Monad.Trans.Reader
 import           Data.Aeson
-import qualified Data.ByteString        as BS
-import qualified Data.ByteString.Base64 as Base64
-import qualified Data.ByteString.Char8  as C8
-import           Data.ByteString.Lazy   (ByteString, fromStrict, toStrict)
-import           Data.Monoid            ((<>))
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Base64     as Base64
+import qualified Data.ByteString.Char8      as C8
+import           Data.ByteString.Lazy       (ByteString, fromStrict, toStrict)
+import           Data.Maybe                 (fromMaybe)
+import           Data.Monoid                ((<>))
 import           Data.Text
 import           Data.Text.Encoding
-import           Data.Text.IO           as TIO
+import           Data.Text.IO               as TIO
 import           Data.Time.Calendar
 import           Data.Time.Clock
 import           Debug.Trace
@@ -37,26 +39,27 @@ import           Kafka.Consumer
     , offsetReset
     , topics
     )
-import qualified Kafka.Producer         as Producer
-import           Kafka.Types            (KafkaLogLevel (..))
-import           Pipes                  (runEffect, (>->))
-import qualified Pipes                  as P
+import qualified Kafka.Producer             as Producer
+import           Kafka.Types                (KafkaLogLevel (..))
+import           Pipes                      (lift, runEffect, (>->))
+import qualified Pipes                      as P
 import           Pipes.Kafka
-import qualified Pipes.Prelude          as P
-import qualified Pipes.Safe             as PS
+import qualified Pipes.Prelude              as P
+import qualified Pipes.Safe                 as PS
+import           System.Environment         (lookupEnv)
 import           System.FilePath
-import           Text.Pandoc hiding (Reader, getCurrentTime)
-import qualified Text.Pandoc.Builder    as PB
-import           Text.Pandoc.Definition (Inline (..), Meta (..))
-import Control.Monad.Trans.Reader
-import Control.Monad.Trans
+import           Text.Pandoc                hiding (getCurrentTime, lookupEnv)
+import qualified Text.Pandoc.Builder        as PB
+import           Text.Pandoc.Definition     (Inline (..), Meta (..))
 
 
 data Config = Config {
-    css :: C8.ByteString
+    css      :: C8.ByteString
   , template :: String
-  , time :: UTCTime
-                     } deriving (Show)
+  , time     :: UTCTime
+  , servers  :: [BrokerAddress]
+  } deriving (Show)
+
 
 processArticle
   :: ConsumerRecord (Maybe C8.ByteString) (Maybe C8.ByteString)
@@ -71,36 +74,44 @@ processArticle ConsumerRecord{crValue} = do
     article2epub Article{..} epub = Epub{..}
     epub64 = decodeUtf8 . Base64.encode . toStrict
     html2epub title author html css template = do
-      Config css template time <- ask
+      Config{css, template, time} <- ask
       lift $ hush $ runPure $ do
-      modifyPureState $ \ s ->
-        s{stFiles = insertInFileTree "epub.css" (FileInfo time css) mempty}
-      readHtml def html
-        >>= setMetadata title author
-        >>= writeEPUB3 def { writerTemplate = (Just template) }
+        modifyPureState $ \ s ->
+          s{stFiles = insertInFileTree "epub.css" (FileInfo time css) mempty}
+        readHtml def html
+          >>= setMetadata title author
+          >>= writeEPUB3 def { writerTemplate = Just template }
     setMetadata title author doc =
       pure $ PB.setTitle (conv title)
       $ PB.setAuthors [maybe (PB.text "unknown") conv author]
-      $ PB.setMeta "css" ("epub.css" :: FilePath) $ doc
-      where conv = PB.text . unpack
+      $ PB.setMeta "css" ("epub.css" :: FilePath) doc
+      where
+        conv = PB.text . unpack
 
 
 main :: IO ()
 main = do
-  css <- BS.readFile "ebook.css"
-  template <- Prelude.readFile "template.t"
-  time <- getCurrentTime
-  let config = Config css template time
-  runNoLoggingT $ PS.runSafeT $ runEffect $ pipe config
-  where
-    pipe config = P.for source (P.each . flip runReaderT config . processArticle) >-> sink
-    sink = kafkaSink producerProps (TopicName "epub")
-    producerProps =
-         Producer.brokersList [BrokerAddress "localhost:9092"]
-      <> Producer.logLevel KafkaLogErr
-    source = kafkaSource consumerProps consumerSub (Timeout 1000)
-    consumerProps =
-      brokersList [BrokerAddress "localhost:9092"] <>
-      groupId (ConsumerGroupId "pubes") <>
-      logLevel KafkaLogErr
-    consumerSub = topics [TopicName "articles"] <> offsetReset Earliest
+  config@Config{servers} <- Config
+    <$> BS.readFile "ebook.css"
+    <*> Prelude.readFile "template.t"
+    <*> getCurrentTime
+    <*> (fmap BrokerAddress
+         . splitOn ","
+         . pack
+         .  fromMaybe "localhost:9092"
+         <$> lookupEnv "KAFKA_BOOTSTRAP_SERVERS")
+
+  let pipe = P.for source (P.each . process) >-> sink
+      process = flip runReaderT config . processArticle
+      sink = kafkaSink producerProps (TopicName "epub")
+      producerProps =
+          Producer.brokersList servers
+        <> Producer.logLevel KafkaLogErr
+      source = kafkaSource consumerProps consumerSub (Timeout 1000)
+      consumerProps =
+        brokersList servers <>
+        groupId (ConsumerGroupId "pubes") <>
+        logLevel KafkaLogErr
+      consumerSub = topics [TopicName "articles"] <> offsetReset Earliest
+
+  runNoLoggingT $ PS.runSafeT $ runEffect pipe
