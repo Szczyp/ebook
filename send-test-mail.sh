c13b43ff0ffaf50a0dc7a2013bd2a03c92d664dc
Szczyp <qb@szczyp.com>
2019-10-17 17:58:16 +0200

    Remove send-test-mail

--- send-test-mail.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-mailsend-go \
--smtp ***REMOVED*** -port 587 \
-auth -user ***REMOVED*** -pass ***REMOVED*** \
--from ***REMOVED*** -to ***REMOVED*** \
--sub "Manage data in Docker" \
-body -msg "https://docs.docker.com/storage/"
