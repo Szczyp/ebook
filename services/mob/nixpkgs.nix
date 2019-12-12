d1a104589f8aa7ebad05131644dd5f50c9fcbe16
Szczyp <qb@szczyp.com>
2019-12-13 00:13:49 +0100

    Kindlegen hack.

---
+++ services/mob/nixpkgs.nix
@@ -0,0 +1,6 @@
+import (builtins.fetchGit {
+  name = "nixpkgs-unstable-small-szczyp-2019-12-13";
+  url = "https://github.com/Szczyp/nixpkgs";
+  ref = "unstable-small";
+  rev = "c8967467cad523cb861df6e07e939030cfd2c558";
+}) {}
