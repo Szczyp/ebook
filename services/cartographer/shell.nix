7211807eccc2140247ebdc9242343e5797a9d159
Szczyp <qb@szczyp.com>
2019-12-14 08:16:11 +0100

    Clean up cartographers default and shell nix. Update mavenix.

--- services/cartographer/shell.nix
+++ services/cartographer/shell.nix
@@ -1,13 +1,1 @@
-{ pkgs ? import ./nixpkgs.nix }:
-with pkgs;
-let
-  mavenix = (callPackage (fetchFromGitHub {
-      owner  = "icetan";
-      repo   = "mavenix";
-      rev    = "v2.3.0";
-      sha256 = "10jyfrc2s45ylff3bw9abvsanrn0xcci8v07b5jn7ibx4y8cwi4c";
-    }) {}).cli;
-in
-mkShell {
-  buildInputs = [ adoptopenjdk-bin maven mavenix ];
-}
+(import ./. {}).shell
