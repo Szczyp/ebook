c42700f1db938253fe58a90588320dba91d37c68
Szczyp <qb@szczyp.com>
2020-01-13 00:44:04 +0100

    Trim node image size.

--- services/lit/default.nix
+++ services/lit/default.nix
@@ -5,49 +5,37 @@ let
 
   nodejs = pkgs.nodejs-12_x;
 
-  rdkafka = with pkgs; stdenv.mkDerivation rec {
-    pname = "rdkafka";
+  rdkafka = pkgs.rdkafka.overrideAttrs (old: rec {
     version = "1.2.2";
-
-    src = fetchFromGitHub {
-      owner = "edenhill";
-      repo = "librdkafka";
-      rev = "v${version}";
+    src = builtins.fetchTarball {
+      url = "https://github.com/edenhill/${old.pname}/archive/v${version}.tar.gz";
       sha256 = "1daikjr2wcjxcys41hfw3vg2mqk6cy297pfcl05s90wnjvd7fkqk";
     };
+  });
 
-    nativeBuildInputs = [ pkgconfig ];
-    buildInputs = [ zlib perl python openssl ];
-
-    NIX_CFLAGS_COMPILE = "-Wno-error=strict-overflow";
-
-    postPatch = ''
-      patchShebangs .
-    '';
-  };
-
-  pnpm2nix = import (pkgs.fetchFromGitHub {
-    owner = "adisbladis";
-    repo = "pnpm2nix";
-    rev = "master";
-    sha256 = "1ck4k4qlwrdvs22ar2hvcn26lj17i481prwa4a684nd344fi191z"; })
+  pnpm2nix = import (builtins.fetchTarball {
+    url = "https://github.com/nix-community/pnpm2nix/archive/b7cb9d9569f0659862f4696fc29a7f7ca4061a75.tar.gz";
+    sha256 = "1nk5mm8psjmq66ggj5flqb154d7yl4j3riy3cdx33yjjbvwvhqh7"; })
     { inherit pkgs nodejs; inherit (pkgs) nodePackages; };
 
   drv = pnpm2nix.mkPnpmPackage {
     src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+    allowImpure = true;
     overrides = pnpm2nix.defaultPnpmOverrides // {
-      node-rdkafka = (drv: drv.overrideAttrs(oldAttrs: {
+      node-rdkafka = (drv: drv.overrideAttrs(old: {
         LIBRDKAFKA=rdkafka;
+        postBuild = ''
+          rm -r /build/node_modules/node-rdkafka/build/Release/obj.target
+          strip /build/node_modules/node-rdkafka/build/Release/node-librdkafka.node
+        '';
       }));
     };
-    allowImpure = true;
   };
 
+  env = pnpm2nix.mkPnpmEnv drv;
+
   shell = pkgs.mkShell {
-    buildInputs = [ nodejs ] ++ (with pkgs.nodePackages; [ pnpm typescript typescript-language-server ]);
-    shellHook = ''
-      export LIBRDKAFKA=${rdkafka}
-    '';
+    buildInputs = [ env ] ++ (with pkgs.nodePackages; [ pnpm typescript typescript-language-server ]);
   };
 
   image = pkgs.dockerTools.buildLayeredImage {
