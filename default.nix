a30f07a6b7bb07a8038cd1caec1617f081f0f5a3
Szczyp <qb@szczyp.com>
2020-01-24 14:36:01 +0100

    1.0.8; pandoc-bin; nixpkgs pin with fetchTarball

--- default.nix
+++ default.nix
@@ -10,8 +10,22 @@ let
     sha256 = "0dbwcb2xsnp9qyx71pghk7p4s1x0zh38qn73h34sgqshwhx23py1";
   }) { inherit pkgs; }).package;
 
+  pandoc-bin = stdenv.mkDerivation {
+    name = "pandoc-bin";
+    propagatedBuildInputs = [ texlive.combined.scheme-basic ];
+    src = fetchTarball {
+      url = https://github.com/jgm/pandoc/releases/download/2.7.3/pandoc-2.7.3-linux.tar.gz;
+      sha256 = "192wxd7519zd6whka6bqbhlgmkzmwszi8fgd39hfr8cz78bc8whc";
+    };
+    buildPhase = "true";
+    installPhase = ''
+    mkdir -p $out/bin
+    cp bin/pandoc $out/bin
+  '';
+  };
+
   external-dependencies = [
-    pandoc
+    pandoc-bin
     kindlegen
     readability
     imagemagick7
--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,11 +1,16 @@
-import (builtins.fetchGit {
-  name = "nixpkgs-unstable-small-szczyp-2019-12-13";
-  url = "https://github.com/Szczyp/nixpkgs";
-  ref = "unstable-small";
-  rev = "c8967467cad523cb861df6e07e939030cfd2c558";
-}) {
+let
+  nixpkgs = builtins.fetchTarball {
+    name = "nixpkgs-unstable-small-szczyp-2019-12-13";
+    url = "https://github.com/Szczyp/nixpkgs/archive/c8967467cad523cb861df6e07e939030cfd2c558.tar.gz";
+    sha256 = "00gx95d093197zb1w2mb0pq83pd5gwglwq5mxam9n7frxxnnj0x9";
+  };
+  poetry2nix = builtins.fetchTarball {
+    url = "https://github.com/adisbladis/poetry2nix/archive/84f27ee31de2d03f525cc80a3675642ecb4377ee.tar.gz";
+    sha256 = "0gn4k7s7zdb4a19086b1faxnvqq34knra39r60c10nirlcjag179";
+  };
+in
+import nixpkgs {
   overlays = [
-    (import ((builtins.fetchTarball {
-      url = https://github.com/adisbladis/poetry2nix/archive/84f27ee31de2d03f525cc80a3675642ecb4377ee.tar.gz; }) + "/overlay.nix"))
+    (import (poetry2nix + "/overlay.nix"))
   ];
 }
